83f14dd0f45165a275279a6b09e67da74e07857f
heyjude101 <ritikraj2884@gmail.com>
2021-02-19 04:49:00 +0530

    improved feature to site wide ban users.

--- app/src/main/java/com/example/kiitanonchat/MainChat.java
+++ app/src/main/java/com/example/kiitanonchat/MainChat.java
@@ -36,13 +36,10 @@ import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.NotificationCompat;
-import androidx.core.app.NotificationManagerCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.firebase.ui.database.FirebaseRecyclerOptions;
-import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -59,8 +56,7 @@ import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
-public class MainChat extends AppCompatActivity
-{
+public class MainChat extends AppCompatActivity {
 
     //uploadButton is what you have to search for.
     Bitmap bitmap;
@@ -75,7 +71,7 @@ public class MainChat extends AppCompatActivity
     Button dialog_button_update;
     LinearLayoutManager mLayoutManager;
     myAdapter adapter;
-    TextView errorMessage , dialog_TextView_update, uniqueIdTvmc;
+    TextView errorMessage, dialog_TextView_update, uniqueIdTvmc;
     ProgressBar pb;
     FirebaseDatabase db;
     model obj;
@@ -102,7 +98,7 @@ public class MainChat extends AppCompatActivity
         mLayoutManager.setStackFromEnd(true);
         recyclerView.setLayoutManager(mLayoutManager);
         pb = findViewById(R.id.simpleProgressBar);
-        errorMessage= findViewById(R.id.errorMessage);
+        errorMessage = findViewById(R.id.errorMessage);
         pb.setVisibility(View.VISIBLE);
 
 
@@ -113,7 +109,7 @@ public class MainChat extends AppCompatActivity
         adapter = new myAdapter(options);
         recyclerView.setAdapter(adapter);
 
-        if(!isConnectedToInternet(getApplicationContext())){
+        if (!isConnectedToInternet(getApplicationContext())) {
             errorMessage.setVisibility(View.VISIBLE);
             pb.setVisibility(View.GONE);
             recyclerView.setVisibility(View.GONE);
@@ -128,8 +124,8 @@ public class MainChat extends AppCompatActivity
             @RequiresApi(api = Build.VERSION_CODES.O)
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                    pb.setVisibility(View.GONE);
-                    recyclerView.setVisibility(View.VISIBLE);
+                pb.setVisibility(View.GONE);
+                recyclerView.setVisibility(View.VISIBLE);
             }
 
             @Override
@@ -141,11 +137,10 @@ public class MainChat extends AppCompatActivity
                 .withPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
                 .withListener(new PermissionListener() {
                     @Override
-                    public void onPermissionGranted(PermissionGrantedResponse response)
-                    {
-                        Intent intent=new Intent(Intent.ACTION_PICK);
+                    public void onPermissionGranted(PermissionGrantedResponse response) {
+                        Intent intent = new Intent(Intent.ACTION_PICK);
                         intent.setType("image/*");
-                        startActivityForResult(Intent.createChooser(intent,"Select Image File"),1);
+                        startActivityForResult(Intent.createChooser(intent, "Select Image File"), 1);
                     }
 
                     @Override
@@ -165,10 +160,12 @@ public class MainChat extends AppCompatActivity
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
             }
+
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
                 b1.setEnabled(s.toString().trim().length() > 0);
             }
+
             @Override
             public void afterTextChanged(Editable s) {
             }
@@ -178,7 +175,7 @@ public class MainChat extends AppCompatActivity
         //SEND BUTTON CLICK HANDLER
         b1.setOnClickListener(v -> {
 
-            if(isConnectedToInternet(getApplicationContext())) {
+            if (isConnectedToInternet(getApplicationContext())) {
                 String val = et1.getText().toString();
                 if (val.contains("$clearChat$")) {
                     node.removeValue();
@@ -218,8 +215,7 @@ public class MainChat extends AppCompatActivity
                         }
                     });
                 }
-            }
-            else{
+            } else {
                 Toast.makeText(this, "Unable to send message. Check Internet connection.", Toast.LENGTH_SHORT).show();
             }
         });
@@ -252,12 +248,11 @@ public class MainChat extends AppCompatActivity
     }
 
 
-
     //MENU OPTION
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_main , menu);
+        inflater.inflate(R.menu.menu_main, menu);
         int positionOfMenuItem = 3;
         MenuItem item = menu.getItem(positionOfMenuItem);
         SpannableString s = new SpannableString("DUMP THIS ACCOUNT");
@@ -265,43 +260,44 @@ public class MainChat extends AppCompatActivity
         item.setTitle(s);
         return true;
     }
+
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         int itemId = item.getItemId();
-        if(itemId == R.id.confess){
-            startActivity(new Intent(getApplicationContext() , ConfessActivity.class));
+        if (itemId == R.id.confess) {
+            startActivity(new Intent(getApplicationContext(), ConfessActivity.class));
         }
-        if(itemId == R.id.about){
-                AlertDialog.Builder alert;
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                    alert = new AlertDialog.Builder(this, android.R.style.Theme_Material_Dialog_Alert);
-                } else {
-                    alert = new AlertDialog.Builder(this);
-                }
-                LayoutInflater inflater = getLayoutInflater();
-                View view = inflater.inflate(R.layout.about_dialog, null);
-                alert.setView(view);
-                alert.setCancelable(true);
-                dialog = alert.create();
-                dialog.getWindow().requestFeature(Window.FEATURE_NO_TITLE);
-                dialog.show();
+        if (itemId == R.id.about) {
+            AlertDialog.Builder alert;
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                alert = new AlertDialog.Builder(this, android.R.style.Theme_Material_Dialog_Alert);
+            } else {
+                alert = new AlertDialog.Builder(this);
+            }
+            LayoutInflater inflater = getLayoutInflater();
+            View view = inflater.inflate(R.layout.about_dialog, null);
+            alert.setView(view);
+            alert.setCancelable(true);
+            dialog = alert.create();
+            dialog.getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+            dialog.show();
         }
-        if(itemId == R.id.logout){
+        if (itemId == R.id.logout) {
             AlertDialog.Builder builder1 = new AlertDialog.Builder(MainChat.this);
             builder1.setView(R.layout.logout_dialog)
                     .setTitle("Are you sure?")
                     .setCancelable(true)
                     .setPositiveButton("Delete", (dialog, which) -> {
                         dialog.dismiss();
-                        SharedPreferences preferences =getSharedPreferences("LogInCheck", Context.MODE_PRIVATE);
+                        SharedPreferences preferences = getSharedPreferences("LogInCheck", Context.MODE_PRIVATE);
                         SharedPreferences.Editor editor = preferences.edit();
                         editor.clear();
                         editor.apply();
-                        SharedPreferences colorPrefs =getSharedPreferences("COLOR", Context.MODE_PRIVATE);
+                        SharedPreferences colorPrefs = getSharedPreferences("COLOR", Context.MODE_PRIVATE);
                         SharedPreferences.Editor editorC = colorPrefs.edit();
                         editorC.clear();
                         editorC.apply();
-                        SharedPreferences usernamePrefs =getSharedPreferences("COLOR", Context.MODE_PRIVATE);
+                        SharedPreferences usernamePrefs = getSharedPreferences("COLOR", Context.MODE_PRIVATE);
                         SharedPreferences.Editor editorUserPrefs = usernamePrefs.edit();
                         editorUserPrefs.clear();
                         editorUserPrefs.apply();
@@ -309,11 +305,11 @@ public class MainChat extends AppCompatActivity
                         finish();
                         Toast.makeText(MainChat.this, "Account deleted and exited the app", Toast.LENGTH_SHORT).show();
                     })
-                    .setNeutralButton("Cancel" , null);
+                    .setNeutralButton("Cancel", null);
             AlertDialog dialog = builder1.create();
             dialog.show();
         }
-        if(itemId == R.id.update){
+        if (itemId == R.id.update) {
             AlertDialog.Builder alert;
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
                 alert = new AlertDialog.Builder(this, android.R.style.Theme_Material_Dialog_Alert);
@@ -321,7 +317,7 @@ public class MainChat extends AppCompatActivity
                 alert = new AlertDialog.Builder(this);
             }
             setTheDesignRealTime();
-            versionC= getSharedPreferences("version" , 0).getString("version" , "0");
+            versionC = getSharedPreferences("version", 0).getString("version", "0");
             LayoutInflater inflater = getLayoutInflater();
             View view = inflater.inflate(R.layout.update_dialog, null);
             dialog_TextView_update = view.findViewById(R.id.dialog_TextView_update);
@@ -333,16 +329,16 @@ public class MainChat extends AppCompatActivity
             dialog.getWindow().requestFeature(Window.FEATURE_NO_TITLE);
             dialog.show();
             dialog_button_update.setOnClickListener(v -> {
-                if(isConnectedToInternet(getApplicationContext())) {
+                if (isConnectedToInternet(getApplicationContext())) {
                     Uri uri = Uri.parse(url);
                     startActivity(new Intent(Intent.ACTION_VIEW, uri));
-                }
-                else {
+                } else {
                     Toast.makeText(MainChat.this, "Check your internet Connection", Toast.LENGTH_SHORT).show();
                 }
             });
         }
-        return true; }
+        return true;
+    }
 
 
     //ACTIVITY LIFECYCLE
@@ -366,26 +362,27 @@ public class MainChat extends AppCompatActivity
             @RequiresApi(api = Build.VERSION_CODES.O)
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                if(snapshot.exists()){
+                if (snapshot.exists()) {
                     dialog_TextView_update.setText("No updates Available at the moment. Please check back later.");
 
-                }
-                else{
+                } else {
                     dialog_TextView_update.setText("Updates are Available. Please click on download button to proceed.");
                     dialog_button_update.setVisibility(View.VISIBLE);
                 }
             }
+
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
             }
         });
     }
-    public void setTheDesignRealTime(){
+
+    public void setTheDesignRealTime() {
         referenceRealTime = db.getReference().child("Realtime");
         referenceRealTime.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                for (DataSnapshot snap: snapshot.getChildren()) {
+                for (DataSnapshot snap : snapshot.getChildren()) {
                     url = (String) snap.child("downloadUrl").getValue();
                 }
             }
@@ -399,7 +396,7 @@ public class MainChat extends AppCompatActivity
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        if(requestCode == 1 && resultCode == RESULT_OK){
+        if (requestCode == 1 && resultCode == RESULT_OK) {
             assert data != null;
             if (data.getData() != null) {
                 filepath = data.getData();
@@ -408,8 +405,8 @@ public class MainChat extends AppCompatActivity
                     bitmap = BitmapFactory.decodeStream(inputStream);
                 } catch (Exception ignored) {
                 }
-                Intent intent = new Intent(MainChat.this , ActivityConfirm.class);
-                intent.putExtra("imageUri" , filepath.toString());
+                Intent intent = new Intent(MainChat.this, ActivityConfirm.class);
+                intent.putExtra("imageUri", filepath.toString());
                 startActivity(intent);
             }
 
